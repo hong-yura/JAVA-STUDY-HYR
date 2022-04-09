1bd8cc9feb03ba7637e3db3dde4a89df4f44ffd5
홍유라 <hhhyr96@gmail.com>
2022-04-09 23:45:27 +0900

    연산자

--- study/src/operator/Operator01.java
+++ study/src/operator/Operator01.java
@@ -1,9 +1,14 @@
 package operator;
 
+// 3-1
 public class Operator01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 2;
+		int y = 5;
+		char c = 'A'; 
+		
+		System.out.println(1 + x << 33);
 
 	}
 
