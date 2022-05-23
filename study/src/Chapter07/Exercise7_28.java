17d2cd68787d94c779768f053a75e94a9b4064b7
user1 <user1@host.docker.internal>
2022-05-23 19:51:36 +0900

    [객체지향2] 문제풀이

--- study/src/Chapter07/Exercise7_28.java
+++ study/src/Chapter07/Exercise7_28.java
@@ -8,14 +8,12 @@ import java.awt.event.*;
 public class Exercise7_28 {
 	public static void main(String[] args) {
 		Frame f = new Frame();
-		f.addWindowListener(new EventHandler());
-	}
-}
-
-class EventHandler extends WindowAdapter {
-	public void windowClosing(WindowEvent e) {
-		e.getWindow().setVisible(false);
-		e.getWindow().dispose();
-		System.exit(0);
-	}
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				e.getWindow().setVisible(false);
+				e.getWindow().dispose();
+				System.exit(0);
+			}
+		});
+	}// main
 }
